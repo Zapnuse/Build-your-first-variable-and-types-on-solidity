edafaa0c9147ecd68161d30a30a093de9ae894c7
zain <113093825+Zapnuse@users.noreply.github.com>
2022-11-01 19:44:55 +0700

    Create courseto.sol

---
+++ courseto.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.4.18;
+
+contract courseto{
+    string  public fname;
+    uint public age;
+    function courseto() public {
+         fname = "anton";
+         age = 20;
+         
+
+    }
+}
